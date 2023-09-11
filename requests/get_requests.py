4adafb8cd91d29762819be8ec487af15b422a3d3
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-11 11:23:34 -0500

    added if statement incase of a 404

--- requests/get_requests.py
+++ requests/get_requests.py
@@ -6,3 +6,10 @@ print(response)
 
 # prints the status code of the get request
 print(response.status_code)
+
+
+# if statement 
+if response.status_code == 200:
+    print('Success!')
+elif response.status_code == 404:
+    print('Not found')
