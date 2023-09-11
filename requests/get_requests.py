ed5e8ee9ebc5f3f2a51d16bdfdac630751c55f2c
Johnnpham7496 <johnpham7496@gmail.com>
2023-09-11 11:25:40 -0500

    added another simpler if statement to check response

--- requests/get_requests.py
+++ requests/get_requests.py
@@ -8,8 +8,15 @@ print(response)
 print(response.status_code)
 
 
-# if statement 
+# if statement to check response status
 if response.status_code == 200:
     print('Success!')
 elif response.status_code == 404:
+    print('Not found')
+
+
+# another type of if statement to check response status
+if response:
+    print('Success!')
+else:
     print('Not found')
